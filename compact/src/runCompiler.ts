9fc5bbee9cccb63e3c3e6eec6c979af959dfcec9
andrew <fleming-andrew@protonmail.com>
2025-08-16 21:23:23 -0300

    add option to compile directory in compact

--- compact/src/runCompiler.ts
+++ compact/src/runCompiler.ts
@@ -12,13 +12,20 @@ import { CompactCompiler } from './Compiler.js';
  * ```bash
  * npx compact-compiler --skip-zk
  * ```
+ *
+ * @example Compile specific directory
+ * ```bash
+ * npx compact-compiler --dir security --skip-zk
+ * ```
+ *
  * Expected output:
  * ```
  * ℹ [COMPILE] Compact compiler started
  * ℹ [COMPILE] COMPACT_HOME: /path/to/compactc
  * ℹ [COMPILE] COMPACTC_PATH: /path/to/compactc/compactc
- * ℹ [COMPILE] Found 1 .compact file(s) to compile
- * ✔ [COMPILE] [1/1] Compiled Foo.compact
+ * ℹ [COMPILE] TARGET_DIR: security
+ * ℹ [COMPILE] Found 1 .compact file(s) to compile in security/
+ * ✔ [COMPILE] [1/1] Compiled security/AccessControl.compact
  *     Compactc version: 0.24.0
  * ```
  */
@@ -26,8 +33,30 @@ async function runCompiler(): Promise<void> {
   const spinner = ora(chalk.blue('[COMPILE] Compact Compiler started')).info();
 
   try {
-    const compilerFlags = process.argv.slice(2).join(' ');
-    const compiler = new CompactCompiler(compilerFlags);
+    const args = process.argv.slice(2);
+
+    // Parse arguments more robustly
+    let targetDir: string | undefined;
+    const compilerFlags: string[] = [];
+
+    for (let i = 0; i < args.length; i++) {
+      if (args[i] === '--dir') {
+        if (i + 1 < args.length && !args[i + 1].startsWith('--')) {
+          targetDir = args[i + 1];
+          i++; // Skip the next argument (directory name)
+        } else {
+          spinner.fail(chalk.red('[COMPILE] Error: --dir flag requires a directory name'));
+          console.log(chalk.yellow('Usage: compact-compiler --dir <directory> [other-flags]'));
+          console.log(chalk.yellow('Example: compact-compiler --dir security --skip-zk'));
+          process.exit(1);
+        }
+      } else {
+        // All other arguments are compiler flags
+        compilerFlags.push(args[i]);
+      }
+    }
+
+    const compiler = new CompactCompiler(compilerFlags.join(' '), targetDir);
     await compiler.compile();
   } catch (err) {
     spinner.fail(
