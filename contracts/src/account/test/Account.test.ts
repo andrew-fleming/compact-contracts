4fa1990a60379dc1aec50d03338c310f9c5a5f62
andrew <fleming-andrew@protonmail.com>
2025-10-27 02:03:59 -0300

    remove unused vars and imports

--- contracts/src/account/test/Account.test.ts
+++ contracts/src/account/test/Account.test.ts
@@ -1,40 +1,12 @@
 import {
-  CoinInfo,
   CompactTypeBytes,
   CompactTypeVector,
-  convertFieldToBytes,
   persistentHash,
 } from '@midnight-ntwrk/compact-runtime';
 import { beforeEach, describe, expect, it } from 'vitest';
-import type { Maybe, ZswapCoinPublicKey } from '../../../artifacts/MockShieldedToken/contract/index.cjs';
 import * as utils from './utils/address.js';
 import { AccountSimulator } from './simulators/AccountSimulator.js';
 
-// PKs
-const [ALICE, zALICE] = utils.generateEitherPubKeyPair('ALICE');
-const [BOB, zBOB] = utils.generateEitherPubKeyPair('BOB');
-const [UNAUTHORIZED, _] = utils.generatePubKeyPair('UNAUTHORIZED');
-
-const NO_STRING: Maybe<string> = {
-  is_some: false,
-  value: '',
-};
-const NAME: Maybe<string> = {
-  is_some: true,
-  value: 'NAME',
-};
-const SYMBOL: Maybe<string> = {
-  is_some: true,
-  value: 'SYMBOL',
-};
-const DECIMALS: bigint = 18n;
-const NONCE: Uint8Array = utils.pad('NONCE', 32);
-const DOMAIN: Uint8Array = utils.pad('ShieldedToken', 32);
-
-const AMOUNT: bigint = BigInt(250);
-const MAX_UINT64 = BigInt(2 ** 64) - BigInt(1);
-
-let secretKey: Uint8Array;
 let account: AccountSimulator;
 
 describe('Account', () => {
