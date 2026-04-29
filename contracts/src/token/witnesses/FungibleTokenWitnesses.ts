918dadcccfb609d4b925e7620afa2a6c9f29aff5
andrew <fleming-andrew@protonmail.com>
2026-04-29 01:06:51 -0300

    fix fmt

--- contracts/src/token/witnesses/FungibleTokenWitnesses.ts
+++ contracts/src/token/witnesses/FungibleTokenWitnesses.ts
@@ -72,6 +72,9 @@ export const FungibleTokenWitnesses = <L>(): IFungibleTokenWitnesses<
   wit_FungibleTokenSK(
     context: WitnessContext<L, FungibleTokenPrivateState>,
   ): [FungibleTokenPrivateState, Uint8Array] {
-    return [context.privateState, Uint8Array.from(context.privateState.secretKey)];
+    return [
+      context.privateState,
+      Uint8Array.from(context.privateState.secretKey),
+    ];
   },
 });
