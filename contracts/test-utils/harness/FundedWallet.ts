7a57741805594dbb924c31f38a204256b16a42a5
0xisk <iskander.andrews@openzeppelin.com>
2026-09-23 11:25:16 +0200

    build(contracts): pin midnight-js/testkit to beta.7 (#939)

--- contracts/test-utils/harness/FundedWallet.ts
+++ contracts/test-utils/harness/FundedWallet.ts
@@ -7,6 +7,7 @@ import {
   syncWallet,
   waitForFunds,
 } from '@midnight-ntwrk/testkit-js';
+import { DustSecretKey, ZswapSecretKeys } from '@midnightntwrk/ledger-v9';
 import { MIN_WALLET_NIGHT, UNDEPLOYED_FEE_OVERHEAD } from './dust.js';
 import type { PooledWallet } from './WalletPool.js';
 
@@ -79,7 +80,8 @@ export class FundedWallet implements PooledWallet {
       logger,
       env,
       wallet,
-      seeds,
+      ZswapSecretKeys.fromSeed(seeds.shielded),
+      DustSecretKey.fromSeed(seeds.dust),
       keystore,
     );
 
